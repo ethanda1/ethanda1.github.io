7c6f2e943a904687d84dfe5ccf6be3c245d0f426
Ethan Dai <dai00171@umn.edu>
2024-11-03 19:48:33 -0600

    fix eslint

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,15 +3,17 @@
 import Navbar from "./Navbar";
 import { TypeAnimation } from 'react-type-animation';
 import { motion } from 'framer-motion';
-import { FaEnvelope } from 'react-icons/fa'
+import { FaEnvelope } from 'react-icons/fa';
 import { useRouter } from "next/navigation";
+
 export default function Home() {
   const router = useRouter();
+  
   return (
     <div className="">
       <Navbar />
       <div className="min-h-screen flex items-center justify-center overflow-hidden">
-        <div className='flex flex-col w-full max-w-5xl items-center mb-40'>
+        <div className="flex flex-col w-full max-w-5xl items-center mb-40">
           <div className="flex justify-start w-full max-w-96 mb-5 pl-10">
             <motion.div
               className="font-light flex text-center mt-5 text-emerald-700"
@@ -32,8 +34,7 @@ export default function Home() {
             </motion.div>
           </div>
 
-          <div className="w-full max-w-300 border-b-2 border-[#ACDDDE]">
-          </div>
+          <div className="w-full max-w-300 border-b-2 border-[#ACDDDE]" />
 
           <motion.div
             className="font-light flex text-center mt-5 text-emerald-700"
@@ -42,12 +43,15 @@ export default function Home() {
             transition={{ duration: 3 }}
           >
             <div className="flex flex-col items-center gap-6">
-              I'm fascinated by large-scale, high-impact products as well as apps that have 100M+ installs.
+              I&apos;m fascinated by large-scale, high-impact products as well as apps that have 100M+ installs.
 
-
-              <div className="flex items-center gap-3 bg-transparent hover:bg-emerald-500 text-emerald-700 font-semibold hover:text-white py-2 px-4 border border-emerald-500 hover:border-transparent rounded w-full max-w-40 select-none" onClick={() => router.push('mailto:ethanydai462@gmail.com')}>
-                <FaEnvelope size='30' />Contact Me
-              </div>
+              <button 
+                className="flex items-center gap-3 bg-transparent hover:bg-emerald-500 text-emerald-700 font-semibold hover:text-white py-2 px-4 border border-emerald-500 hover:border-transparent rounded w-full max-w-40 select-none"
+                onClick={() => router.push('mailto:ethanydai462@gmail.com')}
+              >
+                <FaEnvelope size={30} />
+                Contact Me
+              </button>
             </div>
           </motion.div>
         </div>
